06e1ba8cec677bd420941789d3ea3aa99ad8e532
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-07-24 14:19:12 -0700

    update Item views and model

--- Models/Item.cs
+++ Models/Item.cs
@@ -5,6 +5,8 @@ namespace YeOldeInv.Models
     public class Item
     {
         public int ItemId { get; set; }
+        public string Object { get; set; }
         public string Description { get; set; }
+        public int LevelReq { get; set; }
     }
 }
